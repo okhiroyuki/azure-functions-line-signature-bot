bfd3b453c08dd0a07e914770d646fd5e60ce5acc
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2018-06-03 18:34:09 +0900

    不要な部分の削除

--- QueueTriggerJS1/index.js
+++ QueueTriggerJS1/index.js
@@ -1,11 +1,3 @@
-// module.exports = function (context, myQueueItem) {
-//     context.log('Node.js queue trigger function processed work item', myQueueItem);
-
-//     context.bindings.outputQueueItem = myQueueItem;
-
-//     context.done();
-// };
-
 var https = require("https");
 var url   = require("url");
 
